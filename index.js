a9de98797206189ae0b7b5d611ad97011de82a88
EstherWMaina <mainaesther77@gmail.com>
2022-08-23 12:24:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+function saturdayFun (activity){
+    switch(activity){
+        default:
+            return "This Saturday, I want to roller-skate!"
+        case "bathe my dog":
+            return "This Saturday, I want to bathe my dog!"
+    }
+}
+
+function mondayWork (work){
+    switch(work){
+        default:
+            return "This Monday, I will go to the office."
+        case "work from home":
+            return "This Monday, I will work from home."
+    }
+}
+
+
+function wrapAdjective (var2 = "*"){
+    return function(var1 = "special"){
+        return `You are ${var2}${var1}${var2}!`;
+        };
+
+    }
+
+
